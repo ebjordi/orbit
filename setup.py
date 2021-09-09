f2bb60513a9f20536f646e2ac9156989f1030774
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-09-09 20:21:28 -0300

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='orbit',
+      version='0.7',
+      description='Orbit object from parameters',
+      url='https://github.com/ebjordi/orbit.git',
+      author='Jordi Eguren Brown',
+      author_email='jordi.eguren.brown@gmail.com',
+      py_modules=['orbit'])
+
