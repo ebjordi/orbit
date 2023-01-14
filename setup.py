56bc49e13232ec14891fe615dcf20629b25c0ada
Jordi Eguren Brown <jordi@fcaglp.unlp.edu.ar>
2023-01-14 03:46:41 -0300

    BREAK: move functional implementation to `orbit_f` move functional implementation tests empty orbit.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(name='orbit',
       version='0.7',
-      description='Orbit object from parameters',
+      description='Orbit object from parameters and functional implementations',
       url='https://github.com/ebjordi/orbit.git',
       author='Jordi Eguren Brown',
       author_email='jordi.eguren.brown@gmail.com',
-      py_modules=['orbit'])
+      py_modules=['orbit','orbit_f'])
 
